50ff54995ec54eec3aaa46529c6c5d4ee671bb98
anthonyrct <anthonyteixaira@gmail.com>
2023-11-16 16:55:19 -0300

    16/11/23

---
+++ ProjetoPetShop(Produtos_e_Carrinho)/pagina-de-carrinho-e-produtos/src/app/finalizar/finalizar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-finalizar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './finalizar.component.html',
+  styleUrl: './finalizar.component.css'
+})
+export class FinalizarComponent {
+
+}
